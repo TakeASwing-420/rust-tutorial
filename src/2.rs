f465bdb759f271a559dbab7d187887d4d4407f90
Deep Mondal <dm7041979@gmail.com>
2024-08-20 12:23:33 +0530

    guessing game

--- src/2.rs
+++ src/2.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let a = 5;
-    let b = 10;
-    let sum = a + b;
+    let a: i32 = 5;
+    let b: i32 = 10;
+    let sum: i32 = a + b;
 
     println!("The sum of {} and {} is {}", a, b, sum);
 }
