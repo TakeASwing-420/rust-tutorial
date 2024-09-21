ed4d307519b797ad4505af53ba0d1a465f69c9e2
Deep Mondal <dm7041979@gmail.com>
2024-09-21 17:51:11 +0530

    update

--- src/9.rs
+++ src/9.rs
@@ -5,5 +5,20 @@ fn main() {
     // let r2 = &x; // immutable reference 2
     let r3 = &mut x;
 
-    println!("r3: {}",r3);
+    println!("r3: {}", r3);
+
+    let s = String::from("Hello World");
+    let (s, res) = calculate_length_with_returning_ownership(s); // Getting back ownership
+    println!("length of '{s}' is {res}");
+    let res= calculate_length_with_reference(&s);
+    println!("length of '{s}' is {res}");
+}
+
+fn calculate_length_with_returning_ownership(s: String) -> (String, usize) {
+    let length = s.len();   // calculate the length
+    (s, length)             // return the string and its length as a tuple
+}
+
+fn calculate_length_with_reference(s: &String) -> usize {
+    s.len()  // works with a reference, no need to return ownership
 }
