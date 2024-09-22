6576b144da83b2c4cf129b2dedbaaa83d02aed1c
Deep Mondal <dm7041979@gmail.com>
2024-09-22 19:12:02 +0530

    update

--- src/14.rs
+++ src/14.rs
@@ -4,12 +4,24 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: dbg!(50 * scale),
-    };
+impl Rectangle {
+    fn area(&self) -> u64 {
+        // Cast both width and height to u64 before multiplication
+        (self.width as u64) * (self.height as u64)
+    }
+}
 
-    dbg!(&rect1);
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let r1 = Rectangle {
+        width: 700,
+        height: 800,
+    };
+    println!("The rectangle is {r1:#?}");
+    println!("Area is {} units", r1.area());
 }
