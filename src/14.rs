9c560a67c4f89c09d66f4e1a8ffcdc11baa74c22
Deep Mondal <dm7041979@gmail.com>
2024-09-22 19:56:46 +0530

    update

--- src/14.rs
+++ src/14.rs
@@ -9,10 +9,15 @@ impl Rectangle {
         // Cast both width and height to u64 before multiplication
         (self.width as u64) * (self.height as u64)
     }
+
+    fn square(size: u32)->Self{
+        Self { width: size, height: size }
+    }
 }
 
 impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
+    fn can_hold(&self, other: &Rectangle, message: &str) -> bool {
+        print!("{message}");
         self.width > other.width && self.height > other.height
     }
 }
@@ -22,6 +27,11 @@ fn main() {
         width: 700,
         height: 800,
     };
+
+    let s = Rectangle::square(478);
     println!("The rectangle is {r1:#?}");
     println!("Area is {} units", r1.area());
+    println!("{}",r1.can_hold(&s, "HEllo GuyZ"));
+    println!("Area is {:?} units", s);
+
 }
