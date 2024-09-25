098a8bc8c4ad1a197b589dbc51fcbb30be78608c
Deep Mondal <dm7041979@gmail.com>
2024-09-25 23:08:23 +0530

    update

---
+++ src/16.rs
@@ -0,0 +1,25 @@
+fn main(){
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+
+    /*let mut vola = vec![1, 2, 3, 4, 5];
+
+    let first = &vola[0];
+
+    vola.push(6);
+
+
+    println!("The first element is: {first}");
+    */
+      //Attempting to add an element to a vector while holding a reference to an item
+}
